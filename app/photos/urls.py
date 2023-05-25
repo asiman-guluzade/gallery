15140af6b1f688b3bf2fe478e9649871e23cf4c2
Asiman Guluzade <asiman2711@gmail.com>
2023-05-26 01:51:00 +0400

    New view and url

--- app/photos/urls.py
+++ app/photos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import GalleryView
+from .views import GalleryView, PhotoView
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('', GalleryView.as_view(), name="gallery"),
-    path('photo/<str:pk>', GalleryView.as_view(), name="gallery"),
+    path('photo/<str:pk>', PhotoView.as_view(), name="photo"),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/photos/views.py
+++ app/photos/views.py
@@ -4,5 +4,9 @@ from rest_framework.views import APIView
 # Create your views here.
 
 class GalleryView(APIView):
-    def get(self, request):
-        return render(request, 'photos/index.html')
+    def get(self, request, pk=None):
+        return render(request, 'photos/test.html')
+    
+class PhotoView(APIView):
+    def get(self, request, pk=None):
+        return render(request, 'photos/Photo.html')
