425da0d6cfe29ee8940dd4dfa9432671c0f13a3c
Asiman Guluzade <asiman2711@gmail.com>
2023-05-25 22:34:12 +0400

    basic model and view

--- app/photos/models.py
+++ app/photos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+
 # Create your models here.
