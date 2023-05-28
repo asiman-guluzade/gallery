1291177f81de600741ab9bb7bc25e8fff873b97d
Asiman Guluzade <asiman2711@gmail.com>
2023-05-28 14:26:21 +0400

    views for new urls

--- app/photos/views.py
+++ app/photos/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
+from .models import *
 # Create your views here.
 
 class GalleryView(APIView):
     def get(self, request, pk=None):
-        return render(request, 'photos/test.html')
+        category = request.GET.get('category')
+        if category is None:
+            photos = Image.objects.all()
+        else:
+            photos = Image.objects.filter(category__name=category)
+        categories = Category.objects.filter() 
+        data = {'categories': categories, 'photos': photos}    
+        return render(request, 'photos/test.html', data)
     
 class PhotoView(APIView):
     def get(self, request, pk=None):
-        return render(request, 'photos/Photo.html')
+        photo = Image.objects.get(id=pk)
+
+        return render(request, 'photos/Photo.html', {'photo':photo})
+    
+class AboutView(APIView):
+    def get(self, request):
+        return render(request, 'photos/about.html')
