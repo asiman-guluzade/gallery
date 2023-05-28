9aa9fc530ccd85201a1072ff24aa42b3a5b2d1bd
Asiman Guluzade <asiman2711@gmail.com>
2023-05-28 14:26:32 +0400

    new url

--- app/photos/urls.py
+++ app/photos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GalleryView, PhotoView
+from .views import GalleryView, PhotoView, AboutView
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('', GalleryView.as_view(), name="gallery"),
     path('photo/<str:pk>', PhotoView.as_view(), name="photo"),
+    path('about', AboutView.as_view(), name="about"),
 
 
 ]
